c82c3d6671b4b9a8610ac0d2216545d5c8e7a626
Christine Ibaraki <christine.ibaraki@gmail.com>
2022-08-08 17:10:28 -1000

    compile param list

--- src/compilation_engine.py
+++ src/compilation_engine.py
@@ -59,6 +59,10 @@ class CompilationEngine:
         raise ValueError(error_message)
 
     def compile_class(self):
+        """
+        Compiles a complete class
+        :return: (str) xml output
+        """
         output_str = "<class>\n"
         current_offset = "\t"
         output_str += self._validate_token(self.tokenizer.current_token(),
@@ -72,17 +76,17 @@ class CompilationEngine:
         output_str += self._validate_token(self.tokenizer.current_token(),
                                           {"{"}, "{", current_offset)
 
-        # expect optional class variable declarations followed by optional subroutine declarations
+        # optional class variable declarations
         while not (self.tokenizer.current_token() in SUBROUTINE_OR_CLASS_END):
             if self.tokenizer.current_token() in {"static", "field"}:
                 output_str += self.compile_class_var_dec(current_offset)
 
+        # optional subroutines
+        #while self.tokenizer.current_token() != "}":
         if self.tokenizer.current_token() in SUBROUTINE_DEC_SET:
-            output_str += current_offset + self.compile_subroutine()
-            self.tokenizer.next()
+            output_str += self.compile_subroutine(current_offset)
 
-        # if self.tokenizer.get_current_token() == "}":
-        #     output_str += "\t" + self.tokenizer.get_current_tag() + "\n"
+        # expect "}"
         return output_str
 
     def compile_class_var_dec(self, current_offset):
@@ -93,6 +97,8 @@ class CompilationEngine:
         """
         output_str = current_offset + "<classVarDec>" + "\n"
         new_offset = current_offset + "\t"
+
+        # guaranteed to be static or field
         output_str += new_offset + self.tokenizer.current_tag() + "\n"
         self.tokenizer.next()
 
@@ -104,22 +110,19 @@ class CompilationEngine:
 
         # expect at least one variable name
         output_str += self._validate_token(self.tokenizer.current_type(),
-                                           {IDENTIFIER_TAG},
-                                           "variable name",
+                                           {IDENTIFIER_TAG}, "variable name",
                                            new_offset)
 
         # parse additional variable names
         while self.tokenizer.current_token() != ";":
             if self.tokenizer.current_token() == ",":
                 output_str += self._validate_token(self.tokenizer.current_token(),
-                                                   {","},
-                                                   ",",
+                                                   {","}, ",",
                                                    new_offset)
                 continue
             if self.tokenizer.current_type() == IDENTIFIER_TAG:
                 output_str += self._validate_token(self.tokenizer.current_type(),
-                                                   {IDENTIFIER_TAG},
-                                                   "variable name",
+                                                   {IDENTIFIER_TAG}, "variable name",
                                                    new_offset)
             else:
                 raise ValueError("Expecting variable name")
@@ -133,6 +136,70 @@ class CompilationEngine:
         output_str += current_offset + "</classVarDec>" + "\n"
         return output_str
 
-    def compile_subroutine(self):
-        return ""
+    def compile_subroutine(self, current_offset):
+        """
+        Compiles a complete method, function, or constructor
+        :param current_offset: (str) tab for parent tag
+        :return: (str) xml for class var dec
+        """
+        output_str = current_offset + "<subroutineDec>" + "\n"
+        dec_offset = current_offset + "\t"
 
+        # guaranteed to be constructor, function, or method
+        output_str += dec_offset + self.tokenizer.current_tag() + "\n"
+        self.tokenizer.next()
+
+        # expect void or type (int, char, boolean, classname)
+        output_str += self._validate_token(self.tokenizer.current_type(),
+                                           {KEYWORD_TAG, IDENTIFIER_TAG},
+                                           "keyword or identifier",
+                                           dec_offset)
+
+        # expect a subroutine name
+        output_str += self._validate_token(self.tokenizer.current_type(),
+                                           {IDENTIFIER_TAG}, "subroutine name",
+                                           dec_offset)
+
+        # expect (
+        output_str += self._validate_token(self.tokenizer.current_token(),
+                                           {"("}, "(", dec_offset)
+
+        # parameter list
+        output_str += self.compile_paramater_list(dec_offset)
+
+        # expect )
+        output_str += self._validate_token(self.tokenizer.current_token(),
+                                           {")"}, ")",  dec_offset)
+        # expect {
+        # subroutineBody
+        # expect }
+
+        output_str += current_offset + "</subroutineDec" + "\n"
+        return output_str
+
+    def compile_paramater_list(self, current_offset):
+        """
+        Compiles a (possibly empty) parameter list
+        :param current_offset: (str) tab for parent tag
+        :return:
+        """
+        output_str = current_offset + "<parameterList>" + "\n"
+        var_offset = current_offset + "\t"
+
+        while self.tokenizer.current_token() != ")":
+            if self.tokenizer.current_token() == ",":
+                output_str += self._validate_token(self.tokenizer.current_token(),
+                                                   {","}, ",",
+                                                   var_offset)
+                continue
+            output_str += self._validate_token(self.tokenizer.current_type(),
+                                               {KEYWORD_TAG, IDENTIFIER_TAG},
+                                               "var type",
+                                               var_offset)
+            output_str += self._validate_token(self.tokenizer.current_type(),
+                                               {IDENTIFIER_TAG},
+                                               "var name",
+                                               var_offset)
+
+        output_str += current_offset + "</parameterList>" + "\n"
+        return output_str
